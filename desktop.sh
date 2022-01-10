828e28eb275d4108c4107150f61e152e6f3da5da
joshua <mama27j@gmail.com>
2022-01-11 00:53:44 +0100

    remove smartstock

--- desktop.sh
+++ desktop.sh
@@ -1,8 +1,5 @@
-#rm -r desktop/smartstock
-#rm -r desktop/dist
-#cp -r dist/smartstock desktop
 node desktop-version.js
 cd desktop || return
 npm install
 npm run build:snap
-snapcraft upload --release=stable,edge,beta dist/smartstock_"${PACKAGE_VERSION}"_amd64.snap
+snapcraft upload --release=stable,edge,beta dist/keshatv_"${PACKAGE_VERSION}"_amd64.snap
