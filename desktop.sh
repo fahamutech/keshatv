97b4e5788fc79481d45ffb4e66cf338cd24e1fee
joshua <mama27j@gmail.com>
2022-01-11 00:40:52 +0100

    add desktop build and GitHub workflow

--- desktop.sh
+++ desktop.sh
@@ -5,4 +5,4 @@ node desktop-version.js
 cd desktop || return
 npm install
 npm run build:snap
-snapcraft upload --release=stable,edge,beta dist/smartstock_${PACKAGE_VERSION}_amd64.snap
+snapcraft upload --release=stable,edge,beta dist/smartstock_"${PACKAGE_VERSION}"_amd64.snap
